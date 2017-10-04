09f7543e26ddfcf6d10288383bf4f95eb1241056
Jason <junsitu2@illinois.edu>
2017-10-04 18:08:42 -0500

    add

---
+++ MP0_0/scripts.js
@@ -0,0 +1,22 @@
+function changeToRed() {
+    document.getElementById("profile").style.backgroundColor = "red";
+    console.log("Changing background color to Red.")
+}
+
+function changeToBlue() {
+    document.getElementById("profile").style.backgroundColor = "purple";
+    console.log("Changing background color to Purple.")
+}
+
+function changeToGrey() {
+    document.getElementById("profile").style.backgroundColor = "grey";
+    console.log("Changing background color to Grey.")
+}
+
+function showContent() {
+    document.getElementById("mainContent").style.display = "none";
+}
+
+function hideContent() {
+    document.getElementById("mainContent").style.display = "block";
+}
